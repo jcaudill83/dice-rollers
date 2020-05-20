aae0595783cae93ca9278bb1a08ee8a7cf6c4540
Justin Caudill <jcaudill_2003@yahoo.com>
2020-05-20 10:24:46 -0400

    final iteration....for now

--- main.js
+++ main.js
@@ -1,30 +1,43 @@
 const rollButton = document.querySelector('#roll-button')
-const showButton =document.querySelector('#show-button')
+const showButton = document.querySelector('#show-button')
 let rolls = document.querySelector("#rolls")
+const sum = document.querySelector("#sum")
+const list = document.querySelector("#list")
+const refresh = document.querySelector("#refresh")
+let ferret = 0
 let position = 0
-const dieRolls = []
+const dieRolls = [] //first rubric point
 
-rollButton.addEventListener("click" , function() {
-let input = rolls.value
-let index = 0
+rollButton.addEventListener("click", function () { //second rubric point
+    let input = rolls.value
+    let index = 0 //third rubric point
 
-while (index < input) {
-let potato = Math.floor(Math.random(0) * 6) + 1
-dieRolls.push(potato)
-index += 1
-}
-console.log(dieRolls)
+    while (index < input) {
+        let potato = Math.floor(Math.random(0) * 6) + 1 //fourth rubric point
+        dieRolls.push(potato) //fifth rubric point
+        ferret += potato
+        index += 1
+    }
+    sum.innerHTML = ferret
+    console.log(dieRolls)
 })
 
-showButton.addEventListener("click" , function() {
+//need to add something to be able to show the sum of all dice. sixth rubric point
+
+showButton.addEventListener("click", function () {
+    let index = 0
+
+    // this event is to show what each die is showing
+    //seventh rubric point
+    while (index < dieRolls.length) {
+        let listItem = document.createElement("li")
+        listItem.innerHTML = dieRolls[index]
+        list.appendChild(listItem)
 
 
-while (condition) {
-}
 
-console.log(showButton)
+        index += 1
+    }
+
+
 })
-
-
-
-
